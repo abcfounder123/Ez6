be75f6f5598d684dc3f70d78491fd9c792e490b0
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-01-31 23:24:57 +0700

    day.1

---
+++ 1_data_types.py
@@ -0,0 +1,78 @@
+
+"""
+
+Programming
+1. Imperative paradigm
+2. Declarative paradigm
+
+#################################################
+
+1. Procedural programming
+2. Object-oriented programming
+3. Functional programming
+
+#################################################
+
+Procedural programming
+1. Sequence
+2. Selection
+3. Loop
+4. Function
+
+#################################################
+
+1. Sequence
+   - top
+   - left
+   - parenthesis first
+
+#################################################
+
+program.1
+program.2
+program.3
+program.4
+
+program.1 program.2 program.3 program.4
+
+program.4(program.3(program.2(program.1)))
+
+#################################################
+
+Exercise.1
+
+p1
+p2 p3(p4)
+p5(p6(p7))
+p8(p11) p9 p10(p12)
+
+1 243 765 11,8 9 12,10
+
+#################################################
+
+Exercise.2
+
+1 234 567 89 10 11 12
+
+p1
+p2 p4(p3)
+p7(p6(p5))
+p9(p8) p10 p12(p11)
+
+##################################################################################################
+
+Data types 15
+
+3 + 3 + 8 + 1
+
+1. Character (chr)
+2. Character string (str)
+3. Documentation string (doc)
+
+1. Integer (int)
+2. Floating-point number (float)
+3. Complex number (complex)
+
+"""
+
+
