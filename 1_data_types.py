7e19d4b3da650ebe37c9e970d702ccdbefafbad3
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-02-01 23:38:00 +0700

    day.2

--- 1_data_types.py
+++ 1_data_types.py
@@ -73,6 +73,118 @@ Data types 15
 2. Floating-point number (float)
 3. Complex number (complex)
 
+1. normal list    ->  list, tuple
+2. unique list    ->  set, frozenset
+3. binary list    ->  bytearray, bytes
+7. number list    ->  range
+8. item list      ->  dict
+
+1. condition      ->  bool
+
+#################################################
+
+item = key + value
+
+Dictionary(English to Myanmar)
+
+key      value
+apple      ?
+banana     ?
+
+Dictionary(Myanmar to English)
+
+key      value
+ -       apple
+ -       banana
+
+#################################################
+
+Buit-in Data types 13 + 2 => 15
+
+1. str
+2. int
+3. float
+4. complex
+5. list
+6. tuple
+7. set
+8. frozenset
+9. bytearray
+10. bytes
+11. range
+12. dict
+13. bool
+
+14. memoryview  (not copy, direct access)
+15. NoneTypes
+
+#################################################
+
+Creating data types by name (14)
+
+1. str
+
+2. int
+3. float
+4. complex
+
+5. list
+6. tuple
+
+7. set
+8. frozenset
+
+9. bytearray
+10. bytes
+
+11. range
+12. dict
+
+13. bool
+
+14. memoryview  
+
+#################################################
+
+Creating data types by symbols (11)
+
+1. quotes
+
+2. .
+3. j
+
+4. [ ]     square bracket
+5. ( )     round bracket, parenthesis
+
+6. { }     curly bracket, brace
+
+7. b
+
+8. { }     curly bracket, brace
+
+
+Value is symbol.
+
+1. integer   =>  1  2
+2. boolean   =>  True  False
+3. NoneTypes => None
+
+#################################################
+
+Name only
+1. frozenset
+2. bytearray
+3. range
+4. memoryview
+
+#################################################
+
+Create by name     =>   int(1).__add__(int(2))
+
+Create by symbol   =>   1 + 2
+
+##################################################################################################
+
 """
 
 
