3b84fc1345b300aae6f8f4e4276ef4ab55c35a2b
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-02-22 23:41:54 +0700

    type casting

---
+++ 6_type_casting.py
@@ -0,0 +1,117 @@
+
+"""
+
+Type casting (21) (14 + c o 2 8 16 r e)
+
+1. str()
+
+2. int()
+3. float()
+4. complex()
+
+5. list()
+6. tuple()
+7. set()
+8. frozenst()
+9. bytearray()
+10. bytes()
+11. range()
+12. dict()
+
+13. bool()
+
+14. memoryview()
+
+15. chr -> str   eg. chr(65)    =>   'A'
+16. ord -> int   eg. ord('A')   =>    65
+17. bin -> str  "0b0011"
+18. oct -> str  "0o75"
+19. hex -> str  "0xa"
+20. repr -> str
+21. eval -> program output(15 +)
+
+################################################
+
+c o 2 8 16 r e
+
+c    chracter               chr
+o    ordinal number         ord
+
+2    binary                 bin
+8    octal                  oct
+16   hexadecimal            hex
+
+r    representation string  repr
+e    evaluation             eval
+
+################################################
+
+ordinal number ( first, second )
+
+chr    ordinal number, Unicode
+A  =>  65
+B  =>  66
+a  =>  97
+b  =>  98
+က =>  4096
+ခ  =>  4097
+
+################################################
+
+Numbering system
+
+decimal  bin     oct
+
+0         0       0
+1         1       1
+2        10       2   
+3        11       3
+4       100       4   
+5                 5
+6                 6
+7                 7
+8                10
+9
+
+10                   
+11
+12
+13
+14
+15
+16
+17
+18
+19
+
+
+hex     dec
+0       0
+1       1
+2       2
+3       3
+4       4
+5       5
+6       6
+7       7
+8       8
+9       9
+a      10
+b      11
+c      12
+d      13
+e      14
+f      15
+10     16
+
+
+100    bin   =>  2 * 2 = 4
+100    oct   =>  8 * 8 = 64
+100    dec   =>  10 * 10 = 100
+100    hex   =>  16 * 16 = 256
+
+################################################################################################
+
+
+"""
+
