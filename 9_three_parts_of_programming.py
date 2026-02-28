253fd2f153f296fc88280c212eedf14a24d15aff
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-02-28 23:18:46 +0700

    program flow

---
+++ 9_three_parts_of_programming.py
@@ -0,0 +1,114 @@
+
+"""
+
+1. Three parts of programmimg 
+2. Basic input/output
+3. program flow   
+4. control flow
+
+################################################
+
+1. Three parts of programmimg
+   - input        (mic)(30 35 60 70 90 120) 
+   - processing   ( 60 70 )
+   - output       (speaker)                 
+
+################################################
+
+2. Basic input/output (str)
+   - input(prompt letter)
+   - print(,)
+
+################################################
+
+3. program flow 
+
+################################################
+
+s = int(input("Seconds = "))
+
+m = s // 60     # 1505 min
+sec = s % 60    #  50 sec
+
+h = m // 60     # 25 h
+min = m % 60    # 5 min
+
+day = h // 24   # 1 day
+hour = h % 24   # 1 h
+
+print(f"{day} day, {hour} hour, {min} min {sec} sec")
+
+################################################
+
+1. input()       '90350'
+2. int()          90350
+3. asssign()
+
+4. floor div()
+5. asssign()
+6. mod()
+7. asssign()
+
+8. floor div()
+9. asssign()
+10. mod()
+11. asssign()
+
+12. floor div()
+13. asssign()
+14. mod()
+16. asssign()
+
+17. format()
+18. print()
+
+Day
+Hour
+Min
+Sec
+
+################################################
+
+4. control flow 
+Sec
+Min
+
+################################################
+
+Associativity
+- left-sided bind
+- right-sided bind (1, 2, 10, 14, 15)(e, u, assignment)
+
+left
+2 ** 2 ** 3
+4 ** 3
+64
+
+right
+2 ** 2 ** 3
+2 ** 8
+256
+
+left
+2 / 2 * 3
+1.0 * 3
+3
+
+right
+2 / 2 * 3
+2 / 6
+3.333
+
+################################################
+
+e, u, assignment   <= right sided bind
+
+Exponent      -  1
++, -, ~, not  -  4
+13 + 1        - 14
+
+                19
+
+################################################################################################
+
+"""
